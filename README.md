9201652e854d8e7715bf3497fa51da46e28df33a
tristen <tristen.brown@gmail.com>
2015-01-05 20:45:56 -0500

    Swath of changes
    - Remove jQuery-ui & jQuery as a dep.
    - Add selectall functionality. Closes #2
    - Add LICENCE. Closes #4
    - Fix the choropleth map

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-HCL Colorpicker for data.
+Colorpicker for data.
 ---
 
-All credit goes to [Gregor Aisch](http://vis4.net) and his excellent post on, [How To Avoid Equidistant HSV Colors](http://vis4.net/blog/posts/avoid-equidistant-hsv-colors/?piwik_campaign=rss&piwik_kwd=3199).
-
-To convert/generate colors in a programmatic way, be sure to check out Gregor Aisch's [chroma.js](https://github.com/gka/chroma.js) lib.
+Credit goes to [Gregor Aisch](http://vis4.net) and his excellent post on, [How To Avoid Equidistant HSV Colors](http://vis4.net/blog/posts/avoid-equidistant-hsv-colors/?piwik_campaign=rss&piwik_kwd=3199). To convert/generate colors in a programmatic way check out [chroma.js](https://github.com/gka/chroma.js).
--- app.js
+++ app.js
@@ -11,55 +11,46 @@ function autoscale(canvas) {
   return ctx;
 }
 
-function debounce(func, wait, immediate) {
-  var timeout;
-  return function() {
-    var context = this,
-      args = arguments;
-    var later = function() {
-      timeout = null;
-      if (!immediate) func.apply(context, args);
-    };
-    var callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if (callNow) func.apply(context, args);
-  };
+var timer = null;
+function debounce(fn, delay) {
+  var context = this, args = arguments;
+  clearTimeout(timer);
+  timer = setTimeout(function() {
+    fn.apply(context, args);
+  }, delay);
 }
 
+var Color = chroma.Color;
 var colorspace = {
-  'lch': {
+  'hcl': {
     dimensions: [
-      ['l', 'lightness', 0, 1.7, 0.6],
-      ['c', 'chroma', 0, 5, 1],
       ['h', 'hue', 0, 360, 0],
-    ],
+      ['c', 'chroma', 0, 5, 1],
+      ['l', 'lightness', 0, 1.7, 0.6]],
     axis: [
-      ['hlc', 'hue-lightness'],
-      ['clh', 'chroma-lightness'],
-      ['lch', 'lightness-chroma-hue']
-    ]
+      ['hlc', 'hue-lightness-chroma'],
+      ['clh', 'chroma-lightness-hue'],
+      ['hcl', 'hue-chroma-lightness']]
   }
 };
 
-function Colorpicker() {
+function Colorpicker(cb) {
   var config = {
     sq: 210,
     scale: 2,
-    axis: 'lch',
-    opt: colorspace.lch,
+    axis: 'hcl',
+    opt: colorspace.hcl,
     x: '',
     y: '',
     z: '',
     zval: 1
   };
-  this.init(config);
+  this.init(config, cb);
 }
 
 Colorpicker.prototype = {
-  colorArray: [],
-  init: function(config) {
-
+  init: function(config, cb) {
+    initPosSet = false;
     function getctx(id, autoscale) {
       return document.getElementById(id).getContext('2d');
     }
@@ -75,7 +66,7 @@ Colorpicker.prototype = {
       xyz[config.dy] = y;
       xyz[config.dz] = config.zval;
 
-      var c = chroma(xyz, 'lch');
+      var c = new Color(xyz, 'hcl');
       return c;
     }
 
@@ -127,10 +118,8 @@ Colorpicker.prototype = {
       config.y = axis[1];
       config.z = axis[2];
 
-      var i;
-
-      for (i = 0; i < colorspace.lch.dimensions.length; i++) {
-        var dim = colorspace.lch.dimensions[i];
+      for (var i = 0; i < colorspace.hcl.dimensions.length; i++) {
+        var dim = colorspace.hcl.dimensions[i];
         if (dim[0] === config.x) {
           config.dx = i;
           config.xdim = dim;
@@ -143,19 +132,14 @@ Colorpicker.prototype = {
         }
       }
 
-      $('#sl-z').slider({
-        min: config.zdim[2],
-        max: config.zdim[3],
-        step: config.zdim[3] > 99 ? 1 : 0.01,
-        value: config.zval
-      });
+      d3.select('#slider')
+        .attr('min', config.zdim[2])
+        .attr('max', config.zdim[3])
+        .attr('step', config.zdim[3] > 99 ? 1 : 0.01)
+        .attr('value', config.zval);
 
-      $('lchel[for=val-z]').html(config.zdim[1]);
-
-      d3.select('#sl-z').select('.ui-slider-handle')
-        .attr('data-tooltip', 'Adjust ' + config.zdim[1]);
-
-      d3.select('#sl-val').html('<span>' + $('#sl-z').slider('value') + '</span>' + axis[2]);
+      d3.select('.js-slider-title')
+        .text('Adjust ' + config.zdim[1]);
     }
 
     function setView(state, reset) {
@@ -168,37 +152,37 @@ Colorpicker.prototype = {
 
     function getXY(color) {
       // inverse operation to getColor
-      var lch = color.lch();
-      return [lch[config.dx], lch[config.dy]];
+      var hcl = color.hcl();
+      return [hcl[config.dx], hcl[config.dy]];
     }
 
-    $('#sl-z').slider({
-      range: 'min',
-      step: 0.01,
-      value: 1,
-      slide: function(event, ui) {
-        $('.tooltip').remove();
-        $('#sl-val span').html(ui.value);
-        config.zval = ui.value;
+    d3.select('#slider')
+      .attr('step', 0.01)
+      .attr('value', 1)
+      .on('mousemove', function() {
+        var v = this.value;
+        d3.select('.js-slider-value').text(v);
+        config.zval = v;
         renderColorSpace();
-      }
-    });
+      });
 
-    d3.select('#controls').selectAll('a')
+    d3.select('.js-add')
       .on('click', function() {
         d3.event.preventDefault();
         d3.event.stopPropagation();
-        var operation = d3.select(this).attr('data-type');
-        if (operation === 'add') {
-          swatches = swatches + 1;
+        swatches = swatches + 1;
+        gradient.steps = swatches;
+        showGradient();
+    });
+
+    d3.select('.js-subtract')
+      .on('click', function() {
+        d3.event.preventDefault();
+        d3.event.stopPropagation();
+        if (swatches != 1) {
+          swatches = swatches - 1;
           gradient.steps = swatches;
           showGradient();
-        } else if (operation === 'subtract') {
-          if (swatches != 1) {
-            swatches = swatches - 1;
-            gradient.steps = swatches;
-            showGradient();
-          }
         }
     });
 
@@ -213,44 +197,41 @@ Colorpicker.prototype = {
 
     function showGradient(from) {
       // draw line
-      var colors = [],
-        col_f, col_t, col;
+      var colors = [], col_f, col_t, col;
       var toX = function(v, dim) {
         return Math.round((v - dim[2]) / (dim[3] - dim[2]) * config.sq * config.scale) - 0.5;
       };
-      var a = gradient.handlesize,
-        b = Math.floor(gradient.handlesize * 0.65),
-        i, fx, fy, x, y;
-      var x0 = toX(gradient.from[0], config.xdim) + 10,
-        x1 = toX(gradient.to[0], config.xdim) + 10,
-        y0 = toX(gradient.from[1], config.ydim) + 10,
-        y1 = toX(gradient.to[1], config.ydim) + 10;
+      var a = gradient.handlesize;
+      var b = Math.floor(gradient.handlesize * 0.65);
+      var x0 = toX(gradient.from[0], config.xdim) + 10;
+      var x1 = toX(gradient.to[0], config.xdim) + 10;
+      var y0 = toX(gradient.from[1], config.ydim) + 10;
+      var y1 = toX(gradient.to[1], config.ydim) + 10;
+      var fx, fy, x, y;
 
       var ctx = gradctx;
       ctx.clearRect(0, 0, 600, 600);
 
-      $('.drag.from').css({
-        width: (a * 2) + 'px',
-        height: (a * 2) + 'px',
-        left: (x0 - a) + 'px',
-        top: (y0 - a) + 'px'
-      });
-
-      if (!from) {
-        $('.drag.to').css({
-          width: (a * 2) + 'px',
-          height: (a * 2) + 'px',
+      if (!initPosSet) {
+        d3.select('.drag.from').style({
+          left: (x0 - a) + 'px',
+          top: (y0 - a) + 'px'
+        });
+        d3.select('.drag.to').style({
           left: (x1 - a) + 'px',
           top: (y1 - a) + 'px'
         });
       }
 
+      // The line that connects the two circular
+      // drag controls on the colorpicker.
       ctx.beginPath();
       ctx.strokeStyle = 'rgba(255,255,255,0.25)';
       ctx.moveTo(x0, y0);
       ctx.lineTo(x1, y1);
       ctx.stroke();
 
+      // `from` drag control on the colorpicker.
       ctx.beginPath();
       ctx.strokeStyle = '#fff';
       col_f = getColor(gradient.from[0], gradient.from[1]).hex();
@@ -260,6 +241,7 @@ Colorpicker.prototype = {
       ctx.closePath();
       ctx.stroke();
 
+      // `to` drag control on the colorpicker.
       ctx.beginPath();
       col_t = getColor(gradient.to[0], gradient.to[1]).hex();
       ctx.fillStyle = col_t;
@@ -270,7 +252,7 @@ Colorpicker.prototype = {
 
       colors.push(col_f);
 
-      for (i = 1; i < gradient.steps - 1; i++) {
+      for (var i = 1; i < gradient.steps - 1; i++) {
         fx = gradient.from[0] + (i / (gradient.steps - 1)) * (gradient.to[0] - gradient.from[0]);
         fy = gradient.from[1] + (i / (gradient.steps - 1)) * (gradient.to[1] - gradient.from[1]);
         x = toX(fx, config.xdim[2]) + 10;
@@ -289,12 +271,12 @@ Colorpicker.prototype = {
 
       colors.push(col_t);
       updateSwatches(colors);
-      updateLocation();
-    }
 
-    var updateLocation = debounce(function() {
-      location.href = '#/' + serialize();
-    }, 100);
+      // Update the url hash
+      debounce(function() {
+        location.href = '#/' + serialize();
+      }, 100);
+    }
 
     function updateSwatches(colors) {
       ['#visual-output', '#legend-output'].forEach(function(id) {
@@ -305,24 +287,13 @@ Colorpicker.prototype = {
         output.style('background', String);
       });
 
+      cb(colors);
       var output = d3.select('#code-output')
         .selectAll('span.value').data(colors);
-      output.exit().remove();
-      output.enter().append('span').attr('class', 'value')
-        .on('click', selectHex);
-      output.text(String);
-    }
 
-    function selectHex() {
-      if (document.selection) {
-        var rangeD = document.body.createTextRange();
-        rangeD.moveToElementText(this);
-        rangeD.select();
-      } else if (window.getSelection) {
-        var rangeW = document.createRange();
-        rangeW.selectNode(this);
-        window.getSelection().addRange(rangeW);
-      }
+      output.exit().remove();
+      output.enter().append('span').attr('class', 'value');
+      output.text(String);
     }
 
     function unserialize(hash) {
@@ -330,7 +301,7 @@ Colorpicker.prototype = {
         // default init settings
         return {
           swatches: 6,
-          axis: colorspace.lch.axis[0],
+          axis: colorspace.hcl.axis[0],
           from: [0, 1],
           to: [1, 0.6]
         };
@@ -345,8 +316,8 @@ Colorpicker.prototype = {
       return {
         swatches: Number(parts[1]),
         axis: parts[0],
-        from: getXY(chroma(parts[3])),
-        to: getXY(chroma(parts[4]))
+        from: getXY(new Color(parts[3])),
+        to: getXY(new Color(parts[4]))
       };
     }
 
@@ -358,14 +329,29 @@ Colorpicker.prototype = {
         getColor(gradient.to[0], gradient.to[1]).hex().substr(1);
     }
 
-    $('.drag').draggable({
-      containment: 'parent',
-      drag: function(event, ui) {
-        var from = $(event.target).hasClass('from'),
-          x = ui.position.left + gradient.handlesize - 10,
-          y = ui.position.top + gradient.handlesize - 10,
-          xv = x / (config.sq * config.scale) * (config.xdim[3] - config.xdim[2]) + config.xdim[2],
-          yv = y / (config.sq * config.scale) * (config.ydim[3] - config.ydim[2]) + config.ydim[2];
+    var drag = d3.behavior.drag()
+      .origin(Object)
+      .on('drag', function(d) {
+        initPosSet = true;
+
+        var posX = parseInt(d3.select(this).style('left').split('px')[0], 10);
+        var posY = parseInt(d3.select(this).style('top').split('px')[0], 10);
+
+        // 440 = width of container. 30 = width of drag circle.
+        posX = Math.max(0, Math.min(440 - 30, posX + d3.event.dx));
+        // 440 = height of container. 30 = height of drag circle.
+        posY = Math.max(0, Math.min(440 - 30, posY + d3.event.dy));
+
+        d3.select(this).style({
+          left: posX + 'px',
+          top: posY + 'px'
+        });
+
+        var from = d3.select(this).classed('from');
+        var x = posX + gradient.handlesize - 10;
+        var y = posY + gradient.handlesize - 10;
+        var xv = x / (config.sq * config.scale) * (config.xdim[3] - config.xdim[2]) + config.xdim[2];
+        var yv = y / (config.sq * config.scale) * (config.ydim[3] - config.ydim[2]) + config.ydim[2];
 
         xv = Math.min(config.xdim[3], Math.max(config.xdim[2], xv));
         yv = Math.min(config.ydim[3], Math.max(config.ydim[2], yv));
@@ -377,32 +363,25 @@ Colorpicker.prototype = {
           gradient.to = [xv, yv];
           showGradient();
         }
-      }
     });
+    d3.select('.drag.to').call(drag);
+    d3.select('.drag.from').call(drag);
 
     function axisLinks() {
       var axis_links = d3.select('.axis-select')
-        .selectAll('li')
-        .data(colorspace.lch.axis);
+        .selectAll('a')
+        .data(colorspace.hcl.axis);
 
       axis_links.exit().remove();
-      axis_links.enter().append('li');
-
-      var alink = axis_links.selectAll('a');
-
-      alink.data(function(d) {
-          return [d];
+      axis_links.enter().append('a')
+        .attr('href', '#')
+        .attr('class', function(d) {
+          return 'axis-option block button pad0x unround ' + d[0];
         })
-        .enter().append('a')
-        .attr({
-          'href': '#',
-          'class': function(d) {
-            return 'axis-select ' + d[0];
-          },
-          'data-tooltip': function(d) {
+        .attr('data-tooltip', function(d) {
             return d[1];
           }
-        })
+        )
         .classed('active', function(d) {
           return d[0] == config.axis;
         })
@@ -412,11 +391,12 @@ Colorpicker.prototype = {
         .on('click', function(d) {
           d3.event.preventDefault();
           d3.event.stopPropagation();
+          initPosSet = false;
           updateAxis(d[0]);
           resetGradient();
           renderColorSpace();
           showGradient();
-          d3.selectAll('a.axis-select').classed('active', function(_) {
+          d3.selectAll('a.axis-option').classed('active', function(_) {
             return _[0] == d[0];
           });
         });
@@ -440,12 +420,12 @@ Colorpicker.prototype = {
   }
 };
 
-function choropleth(counties) {
+function choropleth(counties, colors) {
   d3.json('example-data/unemployment.json', function(data) {
-    var quantize = d3.scale.quantile().domain(d3.values(data)).range(d3.range(color.colorArray.length));
+    var quantize = d3.scale.quantile().domain(d3.values(data)).range(d3.range(colors.length));
     d3.json('example-data/us-counties.json', function(json) {
       counties.selectAll('path').data(json.features).enter().append('svg:path').attr('style', function(d) {
-          return 'fill:' + color.colorArray[quantize(data[pad(d.id)])] + ';';
+          return 'fill:' + colors[quantize(data[pad(d.id)])] + ';';
         })
         .attr('d', path).append('svg:title').text(function(d) {
           return d.properties.name + ': ' + data[pad(d.id)] + '%';
@@ -455,43 +435,62 @@ function choropleth(counties) {
   });
 }
 
-function load() {
-  var mode = d3.selectAll('.js-mode');
-  var vizs = d3.select('#visualization');
-  var pick = d3.select('#picker');
+var client = new ZeroClipboard( document.getElementById('select') );
+var selectButton = d3.selectAll('.js-select');
 
-  if (location.hash === '') location.hash = '/hlc/6/1/16534C/E2E062';
-  var color = new Colorpicker();
-  var path = d3.geo.path()
-    .projection(d3.geo.albersUsa()
-      .scale(960)
-      .translate([480, 265]));
-
-  var svg = vizs.append('svg:svg')
-    .attr('width', 960)
-    .attr('height', 500);
-
-  var counties = svg.append('svg:g').attr('id', 'counties');
-  var pad = d3.format('05d');
-
-  mode.on('click', function() {
-    d3.event.preventDefault();
-    d3.event.stopPropagation();
-
-    var el = d3.select(this);
-    mode.classed('active', false);
-    el.classed('active', true);
-
-    if (el.attr('href').split('#')[1] === 'picker') {
-      vizs.classed('hidden', true);
-      pick.classed('hidden', false);
-      counties.selectAll('path').remove();
-    } else {
-      pick.classed('hidden', true);
-      vizs.classed('hidden', false).classed('loading', false);
-      chropleth(counties);
-    }
+client.on('ready', function() {
+  client.on('aftercopy', function(e) {
+    selectButton.text('Copied!');
+    setTimeout(function() {
+      selectButton.text('Select all')
+        .append('span')
+        .attr('class', 'sprite icon clipboard');
+    }, 1000);
   });
-}
+});
 
-$(load);
+var mode = d3.selectAll('.js-mode');
+var vizs = d3.select('#visualization');
+var pick = d3.select('#picker');
+var select = d3.select('.js-select');
+var colorArray = [];
+select.on('click', function() {
+  d3.event.preventDefault();
+  d3.event.stopPropagation();
+});
+
+if (!location.hash) location.hash = '/hlc/6/1/16534C/E2E062';
+var color = new Colorpicker(function(colors) {
+  colorArray = colors;
+  select.attr('data-clipboard-text', colors);
+});
+var path = d3.geo.path()
+  .projection(d3.geo.albersUsa()
+    .scale(960)
+    .translate([480, 265]));
+
+var svg = vizs.append('svg:svg')
+  .attr('width', 960)
+  .attr('height', 500);
+
+var counties = svg.append('svg:g').attr('id', 'counties');
+var pad = d3.format('05d');
+
+mode.on('click', function() {
+  d3.event.preventDefault();
+  d3.event.stopPropagation();
+
+  var el = d3.select(this);
+  mode.classed('active', false);
+  el.classed('active', true);
+
+  if (el.attr('href').split('#')[1] === 'picker') {
+    vizs.classed('hidden', true);
+    pick.classed('hidden', false);
+    counties.selectAll('path').remove();
+  } else {
+    pick.classed('hidden', true);
+    vizs.classed('hidden', false).classed('loading', true);
+    choropleth(counties, colorArray);
+  }
+});
